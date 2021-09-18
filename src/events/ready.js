93f092479ec85f8c3960790488f9e809c9524cd9
Vadim Pashkov <vadimpashkov.job@gmail.com>
2021-09-19 02:14:37 +0500

    Add "ready" event

---
+++ src/events/ready.js
@@ -0,0 +1,13 @@
+export default {
+	once: true,
+	execute(client) {
+		client.user.setUsername('Nagachika Hideyoshi');
+
+		client.user.setPresence({
+			activities: [{ name: 'Bloodhunt', type: 'COMPETING' }],
+			status: 'dnd',
+		});
+
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+	},
+};
