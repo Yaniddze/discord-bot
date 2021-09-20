a840364c710639cc0382a49d68e13e3fc65e3969
Vadim Pashkov <vadimpashkov.job@gmail.com>
2021-09-21 01:28:15 +0500

    Install glob package

--- src/handler/connectingEvents.js
+++ src/handler/connectingEvents.js
@@ -1,13 +1,14 @@
-import { readdir } from 'fs';
+import glob from 'glob';
+import { parse } from 'path';
 
 export const connectingEvents = (client) => {
-	readdir(`./src/events`, (err, files) => {
+	glob('./src/events/**/*.js', (err, files) => {
 		if (err) {
 			console.error(err);
 		} else {
 			files.map(async (file) => {
-				const { default: event } = await import(`../events/${file}`);
-				const eventName = file.replace('.js', '');
+				const { default: event } = await import(`../../${file}`);
+				const eventName = parse(file).name;
 
 				if (event.once) {
 					client.once(eventName, (...args) => event.execute(...args, client));
